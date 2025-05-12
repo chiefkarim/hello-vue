eebd652bc95634350fd6e04abe678b9ef8c2adc6
chiefkarim <krimotop2016@gmail.com>
2025-05-12 19:24:36 +0100

    feat: show movies using tmdb api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Films Populaires</h1>
+    <div v-if="loading">Chargement...</div>
+    <div v-else>
+      <div v-for="movie in movies" :key="movie.id">
+        <h2>{{ movie.title }}</h2>
+        <img :src="getImageUrl(movie.poster_path)" alt="Affiche du film" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const movies = ref([]);
+const loading = ref(true);
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-FR`;
+
+onMounted(async () => {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  movies.value = data.results;
+  loading.value = false;
+});
+
+const getImageUrl = (path) => `https://image.tmdb.org/t/p/w200${path}`;
+</script>
